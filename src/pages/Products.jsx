eb06291b62ff1f6e60b13e5876087a8c35a8222b
Krmanrique <krmanriquesanchez@gmail.com>
2025-10-27 01:07:49 -0500

    fix: se corrigen URLS

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,7 +3,7 @@ import '../styles/AdminDashboard.css';
 import { useNavigate } from 'react-router-dom';
 
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = 'https://back-sales-tau.vercel.app';
 
 const Products = () => {
   const [data, setData] = useState([]);
